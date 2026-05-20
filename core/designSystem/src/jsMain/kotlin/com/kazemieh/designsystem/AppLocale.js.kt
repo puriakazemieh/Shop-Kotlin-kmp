5464bffd99fb37f18cc4f241fe5f7a2366a5e0eb
puriakazemieh <puriakazemieh@gmail.com>
2026-05-20 18:02:35 +0330

    [FEATURE] run in web[1]

--- core/designSystem/src/jsMain/kotlin/com/kazemieh/designsystem/AppLocale.js.kt
+++ core/designSystem/src/jsMain/kotlin/com/kazemieh/designsystem/AppLocale.js.kt
@@ -9,6 +9,21 @@ private val LocalJsLocale = staticCompositionLocalOf { "en" }
 actual object AppLocale {
     @Composable
     actual infix fun provides(value: String): ProvidedValue<*> {
+        val lang = value.replace("_", "-")
+        js(
+            """
+            if (!window.__custom_languages_intercepted) {
+                window.__custom_languages_intercepted = true;
+                var originalLanguagesDescriptor = Object.getOwnPropertyDescriptor(Navigator.prototype, 'languages');
+                Object.defineProperty(Navigator.prototype, 'languages', {
+                    get: function() {
+                        return window.__custom_locale ? [window.__custom_locale] : originalLanguagesDescriptor.get.call(navigator);
+                    }
+                });
+            }
+            window.__custom_locale = lang;
+        """
+        )
         return LocalJsLocale.provides(value)
     }
 }
