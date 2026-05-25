2162743fcd8a130bbcfb8da7a15a3614c727ea17
puriakazemieh <puriakazemieh@gmail.com>
2026-05-25 16:33:03 +0330

    [FEATURE] Revert base url

--- core/network/src/iosMain/kotlin/com/kazemieh/network/PlatformConfig.ios.kt
+++ core/network/src/iosMain/kotlin/com/kazemieh/network/PlatformConfig.ios.kt
@@ -1,5 +1,5 @@
 package com.kazemieh.network
 
 actual object PlatformConfig {
-    actual val baseUrl = "https://womb-nearly-justify.ngrok-free.dev/" // "http://localhost:8080/"
+    actual val baseUrl = "http://localhost:8080/"
 }
