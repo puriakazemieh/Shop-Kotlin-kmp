f83adabe18f99d0cbb67c19f22137d2b067e9e84
puriakazemieh <puriakazemieh@gmail.com>
2026-06-13 11:13:01 +0330

    fix(network): update baseUrl configuration for Android and JS platforms
    - Remove empty AndroidManifest.xml file from admin_panel feature
    - Change Android baseUrl from "api.miaad.puriademo.ir" to "api.milad.puriademo.ir"
    - Update JS baseUrl to "https://api.miaad.puriademo.ir" and comment out previous URLs

--- core/network/src/androidMain/kotlin/com/kazemieh/network/PlatformConfig.android.kt
+++ core/network/src/androidMain/kotlin/com/kazemieh/network/PlatformConfig.android.kt
@@ -3,5 +3,6 @@ package com.kazemieh.network.common
 actual object PlatformConfig {
 //    actual val baseUrl = "http://10.0.2.2:8080/"
 //    actual val baseUrl = "https://shop-server.liara.run/"
-    actual val baseUrl = "https://api.miaad.puriademo.ir/"
+//    actual val baseUrl = "https://api.miaad.puriademo.ir/"
+    actual val baseUrl = "https://api.milad.puriademo.ir/"
 }
--- core/network/src/jsMain/kotlin/com/kazemieh/network/PlatformConfig.js.kt
+++ core/network/src/jsMain/kotlin/com/kazemieh/network/PlatformConfig.js.kt
@@ -1,5 +1,7 @@
 package com.kazemieh.network.common
 
 actual object PlatformConfig {
-    actual val baseUrl = "http://localhost:8080/"
+//    actual val baseUrl = "http://localhost:8080/"
+    actual val baseUrl = "https://api.miaad.puriademo.ir/"
+//    actual val baseUrl = "https://api.milad.puriademo.ir/"
 }
